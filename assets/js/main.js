9ed7805b8c7b70700ee946635cf24bc24ce9d47b
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-07-12 09:33:54 +0000

    deploy: d86e01f93c1d69f7b0c18f1435d80a85c17a6bca

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,67 @@
 
+{
+
+  let mySwiper;
+
+  const swiperAbout = document.querySelector('.about__slider');
+
+      mySwiper = new Swiper(swiperAbout, {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+
+        uniqueNavElements: true,
+
+        // Откл функционала, если слайдов меньше, чем нужно
+        watchOverflow: true,
+
+        //centeredSlides: true,
+
+        // Отступ между слайдами
+        //spaceBetween: 24,
+
+        // Стартовый слайд
+        initialSlide: 0,
+
+        // Брейк поинты (адаптив)
+        // Ширина экрана
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 16,
+          },
+          480: {
+            slidesPerView: 1.3,
+          },
+
+          640: {
+            slidesPerView: 1.5,
+            spaceBetween: 32,
+            initialSlide: 0,
+          },
+
+          1100: {
+            slidesPerView: 3,
+            spaceBetween: 32,
+          },
+        }
+
+      });
+
+
+      // const images = swiper.querySelectorAll('.swiper-slide img');
+      // images.forEach(image => {
+      //   image.setAttribute('data-src', image.src);
+      //   image.setAttribute('data-fancybox', "images-index");
+      // });
+
+
+
+
+
+}
+
 const header = document.querySelector('header.header');
 const menu = header.querySelector('.header__nav');
 const burger = header.querySelector('.header__burger');

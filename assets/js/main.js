864062ee7fa4f19784fb1f9849a68f65dea04aff
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-07-12 07:35:23 +0000

    deploy: 993acd2418d4178e1b4e0615cb4d7f2547343cb0

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+const header = document.querySelector('header.header');
+const menu = header.querySelector('.header__nav');
+const burger = header.querySelector('.header__burger');
+const menuClose = header.querySelector('.header__nav-close');
+
+burger.addEventListener('click', () => {
+  menu.classList.add('js-menu-open');
+  blockScrollBody();
+});
+
+menuClose.addEventListener('click', () => {
+  menu.classList.remove('js-menu-open');
+  unblockScrollBody();
+});
+
+
+
+
+
+const html = document.querySelector('html');
+
+const classBlockScroll = "js-block-scroll";
+
+function blockScrollBody () {
+  if ( !html.classList.contains(classBlockScroll) ) {
+    html.classList.add(classBlockScroll);
+  }
+};
+
+function unblockScrollBody () {
+  if ( html.classList.contains(classBlockScroll) ) {
+    html.classList.remove(classBlockScroll);
+  }
+};
+
+

69a86d9433a07846701482bcb7729480bbede778
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-07-13 11:03:52 +0000

    deploy: 5449f9074784c617e0bf369a02d4a9b3a9043f03

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,16 +49,11 @@
 
       });
 
-
       // const images = swiper.querySelectorAll('.swiper-slide img');
       // images.forEach(image => {
       //   image.setAttribute('data-src', image.src);
       //   image.setAttribute('data-fancybox', "images-index");
       // });
-
-
-
-
 
 }
 
@@ -79,6 +74,57 @@ menuClose.addEventListener('click', () => {
 
 
 
+
+{
+
+  let mySwiper;
+
+  const swiperPopular = document.querySelector('.popular__slider');
+
+  mySwiper = new Swiper(swiperPopular, {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    // Откл функционала, если слайдов меньше, чем нужно
+    watchOverflow: true,
+
+    // Брейк поинты (адаптив)
+    // Ширина экрана
+    breakpoints: {
+      320: {
+        slidesPerView: 1.1,
+        spaceBetween: 16,
+      },
+      380: {
+        slidesPerView: 1.4,
+        spaceBetween: 16,
+      },
+      480: {
+        slidesPerView: "auto",
+        spaceBetween: 16,
+      },
+
+      640: {
+        slidesPerView: "auto",
+        spaceBetween: 32,
+      },
+
+      1100: {
+        slidesPerView: "auto",
+        spaceBetween: 32,
+      },
+    }
+
+  });
+
+}
 
 
 const html = document.querySelector('html');

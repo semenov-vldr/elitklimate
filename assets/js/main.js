6204a7cd769ee499798e0fdbef14fed45c2b9c22
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-07-18 22:22:31 +0000

    deploy: 9c023c7a323d21fe12e88abbbb7baf6f4b4dafba

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,17 +59,66 @@ if (filter) {
     })
   });
 
-
   const filterBtn = filter.querySelector('.filter__icon');
   const closeFilter = filter.querySelector('.filter__close');
 
   filterBtn.addEventListener('click', () => {
-    filterBtn.classList.add('js-filter-open')
+    filterBtn.classList.add('js-filter-open');
+    blockScrollBody();
   });
 
   closeFilter.addEventListener('click', () => {
     filterBtn.classList.remove('js-filter-open');
-  })
+    unblockScrollBody();
+  });
+
+
+
+  // Добавление кнопки на моб.версии---------------
+
+  const form = filter.querySelector('.filter__form');
+  const btnForm = form.querySelector('.filter__form-submit');
+
+
+  const visibleBtn = () => btnForm.classList.add('js-visible');
+  const hiddenBtn = () => btnForm.classList.remove('js-visible');
+
+  //checkboxes----------------------------
+  const checkboxesForm = form.querySelectorAll('input[type="checkbox"]');
+
+  function isSelectedCheckbox (checkboxes) {
+    return Array.from(checkboxes).some(checkbox => checkbox.checked);
+  };
+
+
+  // Selects-----------------
+  const selectsForm = form.querySelectorAll('select');
+
+  function isSelectedOption (selects) {
+    return Array.from(selects).some(select => select.selectedIndex !== 0);
+  };
+
+  // Inputs price
+  const inputsNumberForm = form.querySelectorAll('input[type="number"]');
+
+  function isEnteredPrice (inputsNumber) {
+    return Array.from(inputsNumber).some(inputNumber => inputNumber.value !== "");
+  };
+
+  // Radio
+  const radiosForm = form.querySelectorAll('input[type="radio"]');
+
+  function isSelectedRadio (radioBtns) {
+    return Array.from(radioBtns).some(radioBtn => radioBtn.checked);
+  };
+
+
+  form.addEventListener('change', () => {
+    ( isSelectedCheckbox(checkboxesForm)
+      || isSelectedOption(selectsForm)
+      || isEnteredPrice(inputsNumberForm)
+      || isSelectedRadio(radiosForm) ) ? visibleBtn() : hiddenBtn();
+  });
 
 
 

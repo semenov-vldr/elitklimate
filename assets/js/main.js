6ec29c72b94bc3800c83cd273c58932677a34612
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-08-14 06:43:32 +0000

    deploy: e30333b6ca2bc507619b9ceeccb4cd37dd877504

--- assets/js/main.js
+++ assets/js/main.js
@@ -1596,6 +1596,7 @@ if (filter) {
   const filterOpen = (filter) => filter.classList.add('js-filter-open');
   const filterHide = (filter) => filter.classList.remove('js-filter-open');
 
+  // Показ/скрытие элементов фильтра
   filterItems.forEach(filterItem => {
     filterItem.addEventListener('click', () => {
       filterItem.classList.toggle('js-filter-open');
@@ -1617,12 +1618,12 @@ if (filter) {
   const closeFilter = filter.querySelector('.filter__close');
 
   filterBtn.addEventListener('click', () => {
-    filterOpen(filterBtn)
+    filterOpen(filterBtn);
     blockScrollBody();
   });
 
   closeFilter.addEventListener('click', () => {
-    filterHide(filterBtn)
+    filterHide(filterBtn);
     unblockScrollBody();
   });
 
@@ -1672,6 +1673,59 @@ if (filter) {
       || isEnteredPrice(inputsNumberForm)
       || isSelectedRadio(radiosForm) ) ? visibleBtn() : hiddenBtn();
   });
+
+
+
+
+
+  // ------- Фильтрация ---
+
+  const productsGrid = document.querySelector(".products__grid");
+
+  const filterPrice = form.querySelector(".filter__item--price");
+  const filterArea = form.querySelector(".filter__item--area");
+
+  const filterInverter = form.querySelector(".filter__item--inverter");
+
+  // -- filter price --
+
+  const minPriceInput = filterPrice.querySelector(".filter__price-min");
+  const maxPriceInput = filterPrice.querySelector(".filter__price-max");
+
+  //minPriceInput.addEventListener("input", filterPriceCards);
+  //maxPriceInput.addEventListener("input", filterPriceCards);
+
+  // function filterPriceCards () {
+  //   const minPriceValue = parseFloat(minPriceInput.value); // Введенное значение
+  //   const maxPriceValue = parseFloat(maxPriceInput.value);
+  // }
+
+
+  // -- filter company --
+    const filterCompany = form.querySelector(".filter__item--company");
+    if (filterCompany) {
+      const companyCheckboxes = filterCompany.querySelectorAll("input[type='checkbox']");
+      let selectedCompanies = [];
+      companyCheckboxes.forEach(companyCheckbox => {
+        companyCheckbox.addEventListener('change', function () {
+          if (this.checked) {
+            selectedCompanies.push(this.value);
+          } else {
+            selectedCompanies = selectedCompanies.filter(el => el !== this.value);
+          }
+          const filteredCompanyArray = new Set(selectedCompanies)
+        });
+      });
+    }
+
+
+
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    productsGrid.replaceChildren();
+    renderCard(productsArr.slice(0, 3))
+  });
+
 
 
 
@@ -1795,16 +1849,14 @@ if (popular) {
 
 
 
-const mainProducts = document.querySelector(".products.products--main");
-
-if (mainProducts) {
-
+// Создание массива карточек товара в соответствии с данными из массива объектов
+function renderCard (products) {
+  const productsGrid = document.querySelector(".products__grid");
   const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
-  const productsGrid = mainProducts.querySelector(".products__grid");
 
-  function createCard (products) {
+  if (productsGrid && cardTemplate) {
+
     products.forEach(product => {
-
       const cardItem = cardTemplate.cloneNode(true);
 
       cardItem.querySelector('.card__link').href = product.link;
@@ -1818,53 +1870,62 @@ if (mainProducts) {
 
       productsGrid.appendChild(cardItem);
     });
-  };
+  }
+};
+//---------------
+
+const productsMain = document.querySelector(".products.products--main");
+if (productsMain) renderCard(productsArr);
 
 
+// Отрисовка карточек на странице выбранной категории в соответствии с категорией
+function addProductsOfCategory (blockCategory, category) {
+  if (blockCategory) {
+    const categoryArray = productsArr.filter(card => card.type === category);
+    renderCard(categoryArray);
+  }
+};
 
-  const startProductsArr = productsArr.slice(0, 6);
+const BlockSplitSystems = document.querySelector(".products.products--split-systems");
+const blockCassette = document.querySelector(".products.products--cassette");
+const blockDucted = document.querySelector(".products.products--ducted");
+const blockColumn = document.querySelector(".products.products--column");
+const blockFloorCeiling = document.querySelector(".products.products--floor-ceiling");
+const blockMultiSplitSystems = document.querySelector(".products.products--multi-split-systems");
 
-  // Отрисовка первичных картчоек при загрузке страницы
-  window.addEventListener("load", () => createCard (startProductsArr));
-
-  let loadedCards = 6; // С номерка какой карточки в массиве начать "ленивую" загрузку
-  let cardsPerPage = 6; // Сколько карточек будет прибавляться при "ленивой" загрузке
-
-  function lazyLoadingCards (products) {
-    const windowsHeight = window.innerHeight;
-    const documentHeight = document.body.offsetHeight;
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (windowsHeight + scrollTop >= documentHeight - 500) {
-      const renderedCards = products.slice(loadedCards, loadedCards+cardsPerPage);
-      loadedCards += renderedCards.length;
-
-        createCard (renderedCards);
-
-      //createCard (renderedCards);
-
-      // console.log("products.length", products.length);
-      // console.log("loadedCards", loadedCards);
-    }
-
-    if (loadedCards === products.length) {
-      console.log('stop')
-      return true;
-    }
-
-  };
-
-  window.addEventListener("scroll", () => {
-    lazyLoadingCards(productsArr);
-
-    if (lazyLoadingCards(productsArr)) {
-      console.log('stop stop stop')
-    }
-  });
+addProductsOfCategory(BlockSplitSystems, "split-system");
+addProductsOfCategory(blockCassette, "cassette");
 
 
 
 
-}
+
+  // // Первично загружаются первые 6 карточек
+  // const startProductsArr = productsArr.slice(0, 6);
+  //
+  // // Отрисовка первичных картчоек при загрузке страницы
+  // window.addEventListener("load", () => createCard (startProductsArr));
+  //
+  // let loadedCards = 6; // С номерка какой карточки в массиве начать "ленивую" загрузку
+  // let cardsPerPage = 6; // Сколько карточек будет прибавляться при "ленивой" загрузке
+  //
+  // function lazyLoadingCards (products) {
+  //   const windowsHeight = window.innerHeight;
+  //   const documentHeight = document.body.offsetHeight;
+  //   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  //
+  //   if (windowsHeight + scrollTop >= documentHeight - 500) {
+  //     const renderedCards = products.slice(loadedCards, loadedCards+cardsPerPage);
+  //     loadedCards += renderedCards.length;
+  //       createCard (renderedCards);
+  //   }
+  // };
+  //
+  // window.addEventListener("scroll", () => lazyLoadingCards(productsArr));
+
+
+
+
+
 
 

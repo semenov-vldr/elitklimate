ec1d19e28e0a45157434d8b32e0419e2e36b103b
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-09-02 21:57:19 +0000

    deploy: 89462de8cdc1ba8a6711202460bce03f973fa29d

--- assets/js/main.js
+++ assets/js/main.js
@@ -2953,13 +2953,15 @@ const LG_Dual = [
 
 // ----- Midea Paramount (MSAG1) -----
 
-const functionsMideaParamount = [""];
+const functionsMideaParamount = ["Фильтр высокой плотности", "Фотокаталитический фильтр", "Автоматическое качание заслонки", "Нагрев до 8°C", "Турбо охлаждение", "2 варианта включения",
+"Самоочистка", "Тихий внутренний блок", "Таймер", "Авторестарт", "Контроль влажности", "Ночной режим", "Обнаружение утечки хладагента", "Локальный комфорт Follow Me", "Самодиагностика",
+"i-Remote", "Wi-Fi Control (опция)"];
 
 const imagesMideaParamount =
-  ["./assets/img/catalog/dahatsu/DA-H/01.png",
-    "./assets/img/catalog/dahatsu/DA-H/02.png",
-    "./assets/img/catalog/dahatsu/DA-H/03.png",
-    "./assets/img/catalog/dahatsu/DA-H/04.png"
+  ["./assets/img/catalog/midea/MSAG1/01.jpg",
+    "./assets/img/catalog/midea/MSAG1/02.png",
+    "./assets/img/catalog/midea/MSAG1/03.jpg",
+    "./assets/img/catalog/midea/MSAG1/04.jpg"
   ];
 
 const descriptionMideaParamount = ""
@@ -2967,51 +2969,248 @@ const descriptionMideaParamount = ""
 
 const MIDEA_MSAG1 = [
 
-  // ------ Dahatsu DA-07H ------
+  // ------ Midea MSAG1-07HRN1-I / MSAG1-07HRN1-O ------
   {
-    article: "dahatsu-da-07h",
-    title: "Dahatsu DA-07H",
+    article: "midea-msag1-07hrn1",
+    title: "Midea MSAG1-07HRN1-I / MSAG1-07HRN1-O",
     type: "split-system",
-    company: "Dahatsu",
-    series: "Legend",
+    company: "Midea",
+    series: "Paramount",
     link: "##",
-    imgSrc: imagesDahatsuLegend,
-    price: 0, // Цена
-    area: 22 , // На площадь, м²
-    coolingCapacity: 2.1, // Мощность охлаждения, кВт
-    noise: 24, // Уровень шума, дБ (A)
+    imgSrc: imagesMideaParamount,
+    price: 29900, // Цена
+    area: 23 , // На площадь, м²
+    coolingCapacity: 2.34, // Мощность охлаждения, кВт
+    noise: 26.5, // Уровень шума, дБ (A)
     inverter: false, // Наличие инвертора (да/нет)
 
-    functions: functionsDahatsuLegend,
-    description: descriptionDahatsuLegend, // Описание
+    functions: functionsMideaParamount,
+    description: descriptionMideaParamount, // Описание
 
     // Охлаждение
     cooling: {
-      performance: 2100, // Производительность, Вт
-      powerConsumption: 650, // Потребл. мощность, Вт
+      performance: 2340, // Производительность, Вт
+      powerConsumption: 710, // Потребл. мощность, Вт
     },
     // Обогрев
     heating: {
-      performance: 2200, // Производительность, Вт
-      powerConsumption: 610, // Потребл. мощность, Вт
+      performance: 2340, // Производительность, Вт
+      powerConsumption: 630, // Потребл. мощность, Вт
     },
     powerSupply: powerSupply, // Электропитание
-    maxTrackLength: "20м", // Макс. длина трассы, м
-    maxHeightDifference: "10м", // Макс. перепад высот, м
+    maxTrackLength: 10, // Макс. длина трассы, м
+    maxHeightDifference: 8, // Макс. перепад высот, м
     liquidPipeDiameter: "6,35мм(1/4)",  // Диаметр жидкостной трубы, мм
     gasPipeDiameter: "9,52мм(3/8)", // Диаметр газовой трубы, мм
     // Внутренний блок
     indoorUnit: {
-      noise: 24, // Уровень шума, дБ (A)
-      weight: 8, // Вес, кг
-      size: "690x199x283", // Размеры
+      noise: 26.5, // Уровень шума, дБ (A)
+      weight: 7.4, // Вес, кг
+      size: "729x292x200", // Размеры
     },
     // Внешний блок
     outdoorUnit: {
-      noise: 48, // Уровень шума, дБ (A)
-      weight: 20, // Вес, кг
-      size: "665x280x420", // Размеры
+      noise: "-", // Уровень шума, дБ (A)
+      weight: 24.6, // Вес, кг
+      size: "720x495x270", // Размеры
       freon: "R410A", // Фреон
+      compressorModel: "GMCC",
+    }
+  },
+
+  // ------ Midea MSAG1-09HRN1-I / MSAG1-09HRN1-O ------
+  {
+    article: "midea-msag1-09hrn1",
+    title: "Midea MSAG1-09HRN1-I / MSAG1-09HRN1-O",
+    type: "split-system",
+    company: "Midea",
+    series: "Paramount",
+    link: "##",
+    imgSrc: imagesMideaParamount,
+    price: 32690, // Цена
+    area: 28 , // На площадь, м²
+    coolingCapacity: 2.64, // Мощность охлаждения, кВт
+    noise: 29.5, // Уровень шума, дБ (A)
+    inverter: false, // Наличие инвертора (да/нет)
+
+    functions: functionsMideaParamount,
+    description: descriptionMideaParamount, // Описание
+
+    // Охлаждение
+    cooling: {
+      performance: 2640, // Производительность, Вт
+      powerConsumption: 820, // Потребл. мощность, Вт
+    },
+    // Обогрев
+    heating: {
+      performance: 2780, // Производительность, Вт
+      powerConsumption: 770, // Потребл. мощность, Вт
+    },
+    powerSupply: powerSupply, // Электропитание
+    maxTrackLength: 20, // Макс. длина трассы, м
+    maxHeightDifference: 8, // Макс. перепад высот, м
+    liquidPipeDiameter: "6,35мм(1/4)",  // Диаметр жидкостной трубы, мм
+    gasPipeDiameter: "9,52мм(3/8)", // Диаметр газовой трубы, мм
+    // Внутренний блок
+    indoorUnit: {
+      noise: 29.5, // Уровень шума, дБ (A)
+      weight: 8.5, // Вес, кг
+      size: "729x292x200", // Размеры
+    },
+    // Внешний блок
+    outdoorUnit: {
+      noise: "-", // Уровень шума, дБ (A)
+      weight: 24.9, // Вес, кг
+      size: "720x495x270", // Размеры
+      freon: "R410A", // Фреон
+      compressorModel: "GMCC",
+    }
+  },
+
+  // ------ Midea MSAG1-12HRN1-I / MSAG1-12HRN1-O ------
+  {
+    article: "midea-msag1-12hrn1",
+    title: "Midea MSAG1-12HRN1-I / MSAG1-12HRN1-O",
+    type: "split-system",
+    company: "Midea",
+    series: "Paramount",
+    link: "##",
+    imgSrc: imagesMideaParamount,
+    price: 37190, // Цена
+    area: 35 , // На площадь, м²
+    coolingCapacity: 3.52, // Мощность охлаждения, кВт
+    noise: 28.5, // Уровень шума, дБ (A)
+    inverter: false, // Наличие инвертора (да/нет)
+
+    functions: functionsMideaParamount,
+    description: descriptionMideaParamount, // Описание
+
+    // Охлаждение
+    cooling: {
+      performance: 3520, // Производительность, Вт
+      powerConsumption: 1100, // Потребл. мощность, Вт
+    },
+    // Обогрев
+    heating: {
+      performance: 3660, // Производительность, Вт
+      powerConsumption: 990, // Потребл. мощность, Вт
+    },
+    powerSupply: powerSupply, // Электропитание
+    maxTrackLength: 20, // Макс. длина трассы, м
+    maxHeightDifference: 8, // Макс. перепад высот, м
+    liquidPipeDiameter: "6,35мм(1/4)",  // Диаметр жидкостной трубы, мм
+    gasPipeDiameter: "12,7мм(1/2)", // Диаметр газовой трубы, мм
+    // Внутренний блок
+    indoorUnit: {
+      noise: 28.5, // Уровень шума, дБ (A)
+      weight: 9.3, // Вес, кг
+      size: "802x295x200", // Размеры
+    },
+    // Внешний блок
+    outdoorUnit: {
+      noise: "-", // Уровень шума, дБ (A)
+      weight: 27.1, // Вес, кг
+      size: "720x495x270", // Размеры
+      freon: "R410A", // Фреон
+      compressorModel: "GMCC",
+    }
+  },
+
+  // ------ Midea MSAG1-18HRN1-I / MSAG1-18HRN1-O ------
+  {
+    article: "midea-msag1-18hrn1",
+    title: "Midea MSAG1-18HRN1-I / MSAG1-18HRN1-O",
+    type: "split-system",
+    company: "Midea",
+    series: "Paramount",
+    link: "##",
+    imgSrc: imagesMideaParamount,
+    price: 57990, // Цена
+    area: 55 , // На площадь, м²
+    coolingCapacity: 5.28, // Мощность охлаждения, кВт
+    noise: 30, // Уровень шума, дБ (A)
+    inverter: false, // Наличие инвертора (да/нет)
+
+    functions: functionsMideaParamount,
+    description: descriptionMideaParamount, // Описание
+
+    // Охлаждение
+    cooling: {
+      performance: 5280, // Производительность, Вт
+      powerConsumption: 1640, // Потребл. мощность, Вт
+    },
+    // Обогрев
+    heating: {
+      performance: 5280, // Производительность, Вт
+      powerConsumption: 1460, // Потребл. мощность, Вт
+    },
+    powerSupply: powerSupply, // Электропитание
+    maxTrackLength: 20, // Макс. длина трассы, м
+    maxHeightDifference: 8, // Макс. перепад высот, м
+    liquidPipeDiameter: "6,35мм(1/4)",  // Диаметр жидкостной трубы, мм
+    gasPipeDiameter: "12,7мм(1/2)", // Диаметр газовой трубы, мм
+    // Внутренний блок
+    indoorUnit: {
+      noise: 30, // Уровень шума, дБ (A)
+      weight: 12.3, // Вес, кг
+      size: "971x321x228", // Размеры
+    },
+    // Внешний блок
+    outdoorUnit: {
+      noise: "-", // Уровень шума, дБ (A)
+      weight: 34.8, // Вес, кг
+      size: "765x555x303", // Размеры
+      freon: "R410A", // Фреон
+      compressorModel: "GMCC",
+    }
+  },
+
+  // ------ Midea MSAG1-24HRN1-I / MSAG1-24HRN1-O ------
+  {
+    article: "midea-msag1-24hrn1",
+    title: "Midea MSAG1-24HRN1-I / MSAG1-24HRN1-O",
+    type: "split-system",
+    company: "Midea",
+    series: "Paramount",
+    link: "##",
+    imgSrc: imagesMideaParamount,
+    price: 74890, // Цена
+    area: 75 , // На площадь, м²
+    coolingCapacity: 7.03, // Мощность охлаждения, кВт
+    noise: 39, // Уровень шума, дБ (A)
+    inverter: false, // Наличие инвертора (да/нет)
+
+    functions: functionsMideaParamount,
+    description: descriptionMideaParamount, // Описание
+
+    // Охлаждение
+    cooling: {
+      performance: 7030, // Производительность, Вт
+      powerConsumption: 2190, // Потребл. мощность, Вт
+    },
+    // Обогрев
+    heating: {
+      performance: 7330, // Производительность, Вт
+      powerConsumption: 2030, // Потребл. мощность, Вт
+    },
+    powerSupply: powerSupply, // Электропитание
+    maxTrackLength: 25, // Макс. длина трассы, м
+    maxHeightDifference: 8, // Макс. перепад высот, м
+    liquidPipeDiameter: "9,52мм(3/8)",  // Диаметр жидкостной трубы, мм
+    gasPipeDiameter: "15,9мм(5/8)", // Диаметр газовой трубы, мм
+    // Внутренний блок
+    indoorUnit: {
+      noise: 39, // Уровень шума, дБ (A)
+      weight: 14.7, // Вес, кг
+      size: "1082x337x234", // Размеры
+    },
+    // Внешний блок
+    outdoorUnit: {
+      noise: "-", // Уровень шума, дБ (A)
+      weight: 52.9, // Вес, кг
+      size: "890x673x342", // Размеры
+      freon: "R410A", // Фреон
+      compressorModel: "GMCC",
     }
   },
 
@@ -3036,6 +3235,10 @@ const dataDenko = [
 
 const dataLG = [
   LG_Dual,
+];
+
+const dataMidea = [
+  MIDEA_MSAG1,
 ]
 
 // Общий массив всех товаров
@@ -3043,6 +3246,7 @@ const productsArr = [
   dataDahatsu,
   dataDenko,
   dataLG,
+  dataMidea,
 
 ].flat().flat();
 

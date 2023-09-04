6a204340a4c750fa075e43889aeb4dc1be97e77a
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-09-04 09:48:01 +0000

    deploy: babf8a579473d44b494c5cfd120f8b1bb3baca06

--- assets/js/main.js
+++ assets/js/main.js
@@ -3715,47 +3715,47 @@ if (filter) {
   ];
 
     // -- <sorting cards> --
-    const filterSortingSelect = filter.querySelector(".filter__sorting select");
-    filterSortingSelect.addEventListener("change", sortingCards);
-
-    if (filterSortingSelect) window.addEventListener("load", sortingCards);
-
-    function sortingCards () {
-      const productsGrid = document.querySelector(".products__grid");
-      const productCards = Array.from(document.querySelectorAll(".products .card"));
-
-      // Сначала дешевле
-      const sortingCheaperProductCards = [...productCards].sort((a, b) => {
-        return +a.dataset.price - +b.dataset.price;
-      });
-      // Сначала дороже
-      const sortingExpensiveProductCards = [...productCards].sort((a, b) => {
-        return +b.dataset.price - +a.dataset.price;
-      });
-      // Алфавиту
-      const sortingAlphabetProductCards = [...productCards].sort((a, b) => {
-        return a.dataset.article.localeCompare(b.dataset.article);
-      });
-
-      productsGrid.replaceChildren();
-
-      switch (filterSortingSelect.value) {
-        // Алфавиту
-        case "initial":
-          sortingAlphabetProductCards.forEach(card => productsGrid.appendChild(card));
-          break;
-        // Дешевле
-        case "cheaper":
-          sortingCheaperProductCards.forEach(card => productsGrid.appendChild(card));
-          break;
-        // Дороже
-        case "expensive":
-          sortingExpensiveProductCards.forEach(card => productsGrid.appendChild(card));
-          break;
-        default:
-          sortingAlphabetProductCards.forEach(card => productsGrid.appendChild(card));
-      };
-    };
+    // const filterSortingSelect = filter.querySelector(".filter__sorting select");
+    // filterSortingSelect.addEventListener("change", sortingCards);
+    //
+    // if (filterSortingSelect) window.addEventListener("load", sortingCards);
+    //
+    // function sortingCards () {
+    //   const productsGrid = document.querySelector(".products__grid");
+    //   const productCards = Array.from(document.querySelectorAll(".products .card"));
+    //
+    //   // Сначала дешевле
+    //   const sortingCheaperProductCards = [...productCards].sort((a, b) => {
+    //     return +a.dataset.price - +b.dataset.price;
+    //   });
+    //   // Сначала дороже
+    //   const sortingExpensiveProductCards = [...productCards].sort((a, b) => {
+    //     return +b.dataset.price - +a.dataset.price;
+    //   });
+    //   // Алфавиту
+    //   const sortingAlphabetProductCards = [...productCards].sort((a, b) => {
+    //     return a.dataset.article.localeCompare(b.dataset.article);
+    //   });
+    //
+    //   productsGrid.replaceChildren();
+    //
+    //   switch (filterSortingSelect.value) {
+    //     // Алфавиту
+    //     case "initial":
+    //       sortingAlphabetProductCards.forEach(card => productsGrid.appendChild(card));
+    //       break;
+    //     // Дешевле
+    //     case "cheaper":
+    //       sortingCheaperProductCards.forEach(card => productsGrid.appendChild(card));
+    //       break;
+    //     // Дороже
+    //     case "expensive":
+    //       sortingExpensiveProductCards.forEach(card => productsGrid.appendChild(card));
+    //       break;
+    //     default:
+    //       sortingAlphabetProductCards.forEach(card => productsGrid.appendChild(card));
+    //   };
+    // };
     // -- </sorting cards> --
 
 
@@ -3798,6 +3798,104 @@ if (filter) {
   // -----------------------------------------------------------------------------------------------
 
 }
+
+
+
+
+function filterTabsCards () {
+  const filterTabsBlock = document.querySelector(".filter-tabs");
+  if (!filterTabsBlock) return;
+
+  const filterTabsList = filterTabsBlock.querySelectorAll(".filter-tabs__list");
+  const listOfRenderedCards = document.querySelectorAll(".products__grid .card");
+
+  filterTabsBlock.querySelector(".filter-tabs__count").textContent = listOfRenderedCards.length;
+
+  filterTabsList.forEach(tabList => {
+    const filterTabs = tabList.querySelectorAll(".filter-tabs__item");
+    filterTabs[0].classList.add("js-filter-active");
+    filterTabs.forEach(filterTab => {
+      filterTab.addEventListener("click", () => {
+        filterTabs.forEach(tab => tab.classList.remove("js-filter-active"));
+        filterTab.classList.toggle("js-filter-active");
+      });
+    });
+  });
+
+
+  const companyTabs = filterTabsBlock.querySelectorAll(".filter-tabs__item[data-company]");
+  const classCompanyHidden = 'js-hidden-company';
+  const typeTabs = filterTabsBlock.querySelectorAll(".filter-tabs__item[data-type]");
+  const classTypeHidden = 'js-hidden-type';
+
+
+  companyTabs.forEach(companyTab => {
+    companyTab.addEventListener("click", () => {
+      const tabCompanyTarget = companyTab.dataset.company;
+      listOfRenderedCards.forEach(renderedCard => {
+        if (renderedCard.dataset.company === tabCompanyTarget || tabCompanyTarget === "all") {
+          renderedCard.classList.remove(classCompanyHidden)
+        } else {
+          renderedCard.classList.add(classCompanyHidden)
+        }
+      });
+    });
+  });
+
+
+
+  const visibleCards = document.querySelectorAll(`.products__grid .card:not(.${classCompanyHidden})`);
+
+
+
+  // -- <sorting cards> --
+  const filterSortingSelect = filterTabsBlock.querySelector(".filter-tabs__sorting select");
+  filterSortingSelect.addEventListener("change", sortingCards);
+
+  if (filterSortingSelect) window.addEventListener("load", sortingCards);
+
+  function sortingCards () {
+    const productsGrid = document.querySelector(".products__grid");
+    const productCards = Array.from(document.querySelectorAll(".products .card"));
+
+    // Сначала дешевле
+    const sortingCheaperProductCards = [...productCards].sort((a, b) => {
+      return +a.dataset.price - +b.dataset.price;
+    });
+    // Сначала дороже
+    const sortingExpensiveProductCards = [...productCards].sort((a, b) => {
+      return +b.dataset.price - +a.dataset.price;
+    });
+    // Алфавиту
+    const sortingAlphabetProductCards = [...productCards].sort((a, b) => {
+      return a.dataset.article.localeCompare(b.dataset.article);
+    });
+
+    productsGrid.replaceChildren();
+
+    switch (filterSortingSelect.value) {
+      // Алфавиту
+      case "initial":
+        sortingAlphabetProductCards.forEach(card => productsGrid.appendChild(card));
+        break;
+      // Дешевле
+      case "cheaper":
+        sortingCheaperProductCards.forEach(card => productsGrid.appendChild(card));
+        break;
+      // Дороже
+      case "expensive":
+        sortingExpensiveProductCards.forEach(card => productsGrid.appendChild(card));
+        break;
+      default:
+        sortingAlphabetProductCards.forEach(card => productsGrid.appendChild(card));
+    };
+  };
+  // -- </sorting cards> --
+
+
+
+
+};
 
 
 
@@ -4224,8 +4322,11 @@ function renderCard (products) {
       cardItem.querySelector('.card__data-item-value--inverter').textContent = `${product.inverter ? "Есть" : "Нет"}`;
       productsGrid.appendChild(cardItem);
       cardItem.setAttribute("data-price", product.price);
+      cardItem.setAttribute("data-type", product.inverter);
+      cardItem.setAttribute("data-company", product.company);
     });
     handlerCart();
+    filterTabsCards();
 };
 //---------------
 

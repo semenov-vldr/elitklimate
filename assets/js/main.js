08292b2fff3f6e97f69cdfdd12fe86e5771be29e
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-09-14 07:56:16 +0000

    deploy: f686d0f1116c4a1290a682f78fb97bf41699d0b9

--- assets/js/main.js
+++ assets/js/main.js
@@ -98,6 +98,72 @@ phoneInputs.forEach(input => {
   input.addEventListener("paste", onPhonePaste);
 });
 
+// Отправка данных формы в Телеграм
+const TOKEN = "6642495645:AAH-ok6fz2lzVfvqxbQgNyeKS6OTx3jc0uQ";
+const CHAT_ID = "-1001965199100";
+const URL_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`;
+
+const forms = document.querySelectorAll("form.form");
+if (forms) {
+  forms.forEach(form => form.addEventListener("submit", sendMessageTelegram));
+}
+
+  function sendMessageTelegram (evt) {
+    evt.preventDefault();
+
+    const formPopup = evt.target.closest(".form-popup");
+    const alertSuccess = document.querySelector(".alert.alert--success");
+    const alertError = document.querySelector(".alert.alert--error");
+
+    function alertActive (blockAlert) {
+      blockAlert.classList.add("js-alert-active");
+      setTimeout(() => {
+        blockAlert.classList.remove("js-alert-active");
+      }, 5000);
+      if (formPopup) {
+        formPopup.classList.remove("js-popup-active");
+      }
+      unblockScrollBody();
+    };
+
+    let message = `<b>Заявка Элит Климат</b>\n`;
+    message += `<b>Имя: ${this.name.value} </b>\n`;
+    message += `<b>Телефон: ${this.tel.value} </b>\n`;
+
+    if (formPopup) {
+      const formPopup = evt.target.closest(".form-popup");
+      const productName = formPopup.querySelector(".form-popup-orders__name").textContent;
+      const productPrice = formPopup.querySelector(".form-popup-orders__price-value").textContent;
+      const productQuantity = formPopup.querySelector(".form-popup__quantity-input").value;
+
+      message += `<b>Товар: ${productName} </b>\n`;
+      message += `<b>Цена: ${productPrice} </b>\n`;
+      message += `<b>Кол-во: ${productQuantity} </b>\n`;
+      message += `<b>Текст: ${this.text.value} </b>\n`;
+    }
+
+    axios.post(URL_API, {
+      chat_id: CHAT_ID,
+      parse_mode: "html",
+      text: message,
+    })
+      .then(res => {
+        console.log("Заявка отправлена");
+        alertActive(alertSuccess);
+      })
+      .catch(err => {
+        console.warn(err);
+        alertActive(alertError);
+      })
+      .finally(() => {
+        console.log("Конец");
+      });
+
+    this.reset();
+  };
+
+
+
 const powerSupply = "220-240V,50HZ";
 const powerSupplyOnePhase = "1 ФАЗА 220-240V,50HZ";
 const powerSupplyThreePhase = "3 ФАЗА 380-415V,50HZ";
@@ -5327,7 +5393,6 @@ function sortingCards (filterSortingSelect) {
       sortingAlphabetProductCards.forEach(card => productsGrid.appendChild(card));
   };
 };
-
 
 const header = document.querySelector('header.header');
 
